c9040f937111eedda690c053389d57fbc2c74e7e
arunima-adhikary7 <aarunima790@gmail.com>
2025-10-25 12:37:08 +0530

    Triangle

---
+++ Core_java/Triangle.java
@@ -0,0 +1,17 @@
+package Core_java;
+
+public class Triangle {
+
+    public static void main(String[] args) {
+        int a=5,b=7,c=10;
+        if(a+b>c && b+c>a && c+a>b)
+        {
+            System.out.println("Triangle is valid");
+        }
+        else
+        {
+            System.out.println("Triangle is not valid");
+        }
+    }
+    
+}

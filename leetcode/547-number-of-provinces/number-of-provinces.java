fb37bd12af9726f8839da9f81f40327c20be1db4
arunima-adhikary7 <aarunima790@gmail.com>
2026-09-11 13:53:41 +0530

    Time: 1 ms (85.80%) | Memory: 47.4 MB (43.53%) - LeetSync

--- leetcode/547-number-of-provinces/number-of-provinces.java
+++ leetcode/547-number-of-provinces/number-of-provinces.java
@@ -1,30 +1,79 @@
 class Solution {
-    int count=0;
-    public int findCircleNum(int[][] isConnected) {
+    // int count=0;
+   
 
-     boolean[] visited=new boolean[isConnected.length];
+    //  boolean[] visited=new boolean[isConnected.length];
     
-    for(int i=0;i<isConnected.length;i++)
-    {
-        if(!visited[i])
-        {
-         dfs(isConnected,visited,i);
-            count++;
-        }
-    }
+    // for(int i=0;i<isConnected.length;i++)
+    // {
+    //     if(!visited[i])
+    //     {
+    //      dfs(isConnected,visited,i);
+    //         count++;
+    //     }
+    // }
   
-     return count;
+    //  return count;
         
-    }
-    private void dfs(int[][] isConnected,boolean[] visited, int node)
-    {
+    // }
+    // private void dfs(int[][] isConnected,boolean[] visited, int node)
+    // {
         
-         visited[node]=true;
+    //      visited[node]=true;
        
-         for(int i=0;i<isConnected.length;i++)
-         {
-            if(isConnected[node][i]==1 && !visited[i])
-               dfs(isConnected,visited,i);
-         }
-    }
+    //      for(int i=0;i<isConnected.length;i++)
+    //      {
+    //         if(isConnected[node][i]==1 && !visited[i])
+    //            dfs(isConnected,visited,i);
+    //      }
+    // }
+
+
+
+
+
+
+
+
+
+int count=0;
+     public int findCircleNum(int[][] isConnected) {
+
+
+       boolean vis[]=new boolean[isConnected.length];
+
+        // Find every new province
+
+        for(int i=0;i<isConnected.length;i++)
+        {
+            if(!vis[i])
+            {
+                dfs(isConnected,vis,i);
+                count++;
+            }
+
+        }
+
+  return count;
+
+
+     }
+
+     private void dfs(int[][] isConnected,boolean[] vis, int node)
+     {
+         // Current city is visited
+        vis[node]=true;
+          // Find all neighbors of current city
+        for(int i=0;i<isConnected.length;i++)
+        {
+            if(isConnected[node][i]==1 && !vis[i])
+            {
+                // Go deeper into that neighbor
+
+                dfs(isConnected,vis,i);
+            }
+        }
+
+     }
+     
 }
